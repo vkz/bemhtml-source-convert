5def7c3ad098cb18c425ccfd5e44f4501adddfc8
vkz <vladilen.kozin@gmail.com>
2014-07-14 14:19:09 +0400

    Classify body.

--- test/scratch-parse2.js
+++ test/scratch-parse2.js
@@ -9,7 +9,8 @@ var convert = require('..'),
     bemparser = require('../lib/ometa/bemhtml').BEMHTMLParser,
     bemtoxjst = require('../lib/ometa/bemhtml').BEMHTMLToXJST,
     parser = require('../lib/ometa/bemhtml-bh').Parser,
-    classifier = require('../lib/ometa/bemhtml-bh').Classifier ;
+    classifier = require('../lib/ometa/bemhtml-bh').Classifier,
+    util = require('util');
 
 var projectRoot = path.resolve(path.join(__dirname, '..')),
     casesRoot = path.join(projectRoot, 'test', 'cases'),
@@ -40,22 +41,60 @@ function classify(src) {
 
 
 // var temp = stx.get(function () {/*
-//     block button {
-//         tag: 'button'
-//         content: 'text'
-//         custommode: applyNext()
-//         this._bla && this._bla === 'bla' { tag: 'a'}
-//     }
+// block b1 {
+//     content: [applyNext(), 'text2']
+// }
 // */});
 
+
 var temp = stx.get(function () {/*
- block button {
-    this.block === 'button': {'tag' : 'button'}
-}*/});
+block button, tag: 'button'
+*/});
+
+// var temp = stx.get(function () {/*
+// block button {
+//     tag: 'button'
+//     this.ctx.url {
+//         tag: 'a'
+//         attrs: {href: this.ctx.url}
+//     }
+// }
+// */});
+
+
+// var temp = stx.get(function () {/*
+// block button {
+//     tag, ~['mark', 'item', 'text'].indexOf(this.elem): 'span'
+// }
+// */});
+
+
+// var temp = stx.get(function () {/*
+// block logo {
+//     this.ctx.url, tag: 'a'
+//     attrs {
+//         lo.isEmpty(this) || true: {role: 'logo'}
+//         this.ctx.url: {
+//             var ctx = this.ctx,
+//             p = applyNext(),
+//             a = { href: ctx.url };
+//             return this._.extend(p, a);}
+//     }
+// }
+// */});
+
+// var temp = stx.get(function () {/*
+// block b1: {
+//     return {a: 1, b: 2}
+// }
+// */});
 
 //parser.matchAll("this.ctx", 'asgnExpr');
 //parse(temp);
-classify(temp);
+var ast = classify(temp);
+console.log(util.inspect (ast, {depth : 300}));
+
+//ast.forEach(function(t){ utils.pp (t[2]);});
 //parse(temp);
 
 //utils.pp(bemparser.matchAll(temp, 'topLevel'));
