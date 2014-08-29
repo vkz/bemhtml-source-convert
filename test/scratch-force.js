4310a776e9160611d7560197adc970a130e8fa9c
vkz <vladilen.kozin@gmail.com>
2014-08-29 13:41:53 +0400

    basic indenting

--- test/scratch-force.js
+++ test/scratch-force.js
@@ -39,7 +39,8 @@ var options = {
 // // return applyNext
 // var bemhtmlsrc = '/Users/kozin/Documents/bh-migration-test/blocks/navigation/_more-type/navigation_more-type_tablo.bemhtml',
 
-var bemhtmlsrc = '/Users/kozin/Documents/bh-migration-test/blocks/navigation/_more-type/navigation_more-type_tablo.bemhtml',
+// var bemhtmlsrc = '/Users/kozin/Documents/bh-migration-test/blocks/navigation/navigation.bemhtml',
+var bemhtmlsrc = 'scratch.bemhtml',
 
     stx = new Stx(fs.readFileSync(bemhtmlsrc, 'utf8')),
     dirname = path.dirname(bemhtmlsrc),
